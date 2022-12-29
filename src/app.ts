e39e42c307f57220d7ddcaf0b6d48822a9296f07
Stephen Brian King <3913213+sbking@users.noreply.github.com>
2022-12-29 12:33:27 -0700

    feat: add helmet

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import helmet from "helmet";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// https://expressjs.com/en/advanced/best-practice-security.html#use-helmet
+app.use(helmet());
+app.disable("x-powered-by");
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
